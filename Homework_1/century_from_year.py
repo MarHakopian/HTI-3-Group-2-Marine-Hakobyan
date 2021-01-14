e0edc437a16418d45b9f9eda75708aa06b9f8fab
marhakopian <marhakopian@gmail.com>
2021-01-14 21:51:36 +0400

    new one

--- Homework_1/century_from_year.py
+++ Homework_1/century_from_year.py
@@ -1,6 +1,7 @@
 
 year = int(input("Enter a year: "))
-century = year // 100 + 1
-if 1 <= year <= 2021:
+century = year // 100
+if year % 100 == 0:
     print("Century - ", century)
-
+else:
+    print("Century - ", century + 1)
